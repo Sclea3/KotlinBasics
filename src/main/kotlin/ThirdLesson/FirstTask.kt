7adb3ffed8e27c400bb58035f829ec3ccfb969f9
sclea3 <sclea3@yandex.ru>
2023-03-23 20:27:32 +0600

    KB-3-1

--- src/main/kotlin/ThirdLesson/FirstTask.kt
+++ src/main/kotlin/ThirdLesson/FirstTask.kt
@@ -1,2 +1,13 @@
 package ThirdLesson
 
+fun main(){
+    val userName = "Alex"
+    var greeting = "Good morning, "
+
+    println(greeting + userName)
+
+    greeting = "Good evening, "
+
+    println(greeting + userName)
+}
+
