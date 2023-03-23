93ebe80c989e01ddba20f271c5057d0a824ce304
sclea3 <sclea3@yandex.ru>
2023-03-24 01:19:18 +0600

    KB-2-2

--- src/main/kotlin/SecondLesson/SecondTask.kt
+++ src/main/kotlin/SecondLesson/SecondTask.kt
@@ -4,13 +4,14 @@ fun main() {
 
     val countOfEmployees = 50
     val salaryOfEachEmployee = 30000
-    val countOfTrainees = 20
+    val countOfTrainees = 30
     val salaryOfEachTrainee = 20000
     val totalSalaryOfEmployees = countOfEmployees * salaryOfEachEmployee
     val totalSalaryOfTrainees = countOfTrainees * salaryOfEachTrainee
+    val totalSalary = totalSalaryOfEmployees + totalSalaryOfTrainees
     val averageSalary = (totalSalaryOfEmployees + totalSalaryOfTrainees) / (countOfTrainees + countOfEmployees)
 
     println("Выплаты постоянным сотрудникам: $totalSalaryOfEmployees RUB")
-    println("Выплаты стажерам: $totalSalaryOfTrainees RUB")
+    println("Общие выплаты: $totalSalary RUB")
     println("Средняя выплата на одного сотрудника: $averageSalary RUB")
 }
