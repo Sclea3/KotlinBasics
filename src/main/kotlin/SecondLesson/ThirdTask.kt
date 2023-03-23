17973289b4af82010d755d9321e5231a6ee4d32e
sclea3 <sclea3@yandex.ru>
2023-03-23 18:42:27 +0600

    KB-2-3

--- src/main/kotlin/SecondLesson/ThirdTask.kt
+++ src/main/kotlin/SecondLesson/ThirdTask.kt
@@ -1,2 +1,16 @@
 package SecondLesson
 
+fun main(){
+
+    val hoursStartTime = 9
+    val minutesStartTime = 39
+    val fullWayInMinutes = 457
+
+    val startTimeInMinutes = hoursStartTime * 60 + minutesStartTime
+    val endTimeInMinutes = startTimeInMinutes + fullWayInMinutes
+
+    val endTimeHours: Int = endTimeInMinutes / 60
+    val endTimeMinutes = endTimeInMinutes % 60
+
+    println("End time is $endTimeHours:$endTimeMinutes")
+}
