0a87bfd2dd32ce5311515e4dce882789720a08ba
sclea3 <sclea3@yandex.ru>
2023-03-25 13:32:15 +0600

    KB-3-5

--- src/main/kotlin/ThirdLesson/FifthTask.kt
+++ src/main/kotlin/ThirdLesson/FifthTask.kt
@@ -1,2 +1,15 @@
 package ThirdLesson
 
+fun main() {
+
+    val moveString = "D2-D4;0"
+
+    val fromSquare = moveString.substringBefore("-")
+    val toSquare = moveString.substringAfter("-").substringBefore(";")
+    val moveNumber = moveString.substringAfter(";").toInt()
+
+    println("Откуда: $fromSquare")
+    println("Куда: $toSquare")
+    println("Номер хода: $moveNumber")
+}
+
